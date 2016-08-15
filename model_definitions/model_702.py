74f607081f944adbe4e55e373ac31ad681d64795
Christophoros Vassiliou <vassiliou@users.noreply.github.com>
2016-08-15 08:49:06 -0700

    100:1 model definitions

---
+++ model_definitions/model_702.py
@@ -0,0 +1,9 @@
+from model_701 import *
+
+model_name = '702'
+
+checkpath = '/home/chrisv/code/model-checkpoints/model_701_log/model.ckpt-39999'
+
+weights = [100,1]
+
+max_steps = 20000
---
+++ model_definitions/model_902.py
@@ -0,0 +1,8 @@
+from model_901 import *
+
+model_name = '902'
+
+checkpath = '/home/chrisv/code/model-checkpoints/model_901_log/model.ckpt-25000'
+
+weights = [100,1]
+max_steps = 15000
