985bfb7edd8693b79c85cb72e9d2f8b112d4a5ba
Christophoros Vassiliou <vassiliou@users.noreply.github.com>
2016-08-17 11:53:18 -0700

    New models split by DBSCAN (x10, x11) and Agglomerative Clustering (x20, x21, x22)

--- runner.bash
+++ runner.bash
@@ -1,5 +1,13 @@
 # model runner
 
-python aws_fcn_train.py 704
-python aws_fcn_train.py 804
-python aws_fcn_train.py 904
+python aws_fcn_train.py 810
+python aws_fcn_train.py 811
+python aws_fcn_train.py 820
+python aws_fcn_train.py 821
+python aws_fcn_train.py 822
+
+python aws_fcn_train.py 910
+python aws_fcn_train.py 911
+python aws_fcn_train.py 920
+python aws_fcn_train.py 921
+python aws_fcn_train.py 922
