c1bb6a0db595781e9f3521ef9cd1dae9538c85b9
Christophoros Vassiliou <vassiliou@users.noreply.github.com>
2016-08-17 11:54:02 -0700

    ...and the model files for DBSCAN, AggClustering

---
+++ model_definitions/model_820.py
@@ -0,0 +1,9 @@
+from model_801 import *
+
+model_name = '820'
+
+checkpath = None #'/home/chrisv/code/model-checkpoints/model_804_log/model.ckpt-9000'
+
+weights = [100,1]
+max_steps = 5000
+randomize = False
---
+++ model_definitions/model_922.py
@@ -0,0 +1,9 @@
+from model_901 import *
+
+model_name = '922'
+
+checkpath = None #'/home/chrisv/code/model-checkpoints/model_804_log/model.ckpt-9000'
+
+weights = [100,1]
+max_steps = 5000
+randomize = False
